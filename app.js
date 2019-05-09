d6a3c7822691acd91b89599112e700b75f062385
WenChaoGit <wenchaoyang@outlook.com>
2019-05-09 10:07:36 +0800

    a

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const Koa = require('koa')
 const Router = require('koa-router')
 const init = require('./app/core/init')
-const app = new Koa()
-const router = new Router()
-
+const app = new Koa();
 init.initCore(app)
 
 app
-  .use(router.routes())
   .listen(3000)
--- app/core/init.js
+++ app/core/init.js
@@ -1,14 +1,14 @@
-
 const requireDirectory = require('require-directory')
+const Router = require('koa-router')
 
 class InitManager {
   static initCore(app){
     InitManager.app = app
-
+    InitManager.initLoadRouters()
   }
 
   static initLoadRouters(){
-    let apiDirectory = `${__dirname}/app/api`
+    let apiDirectory = `${process.cwd()}/app/api`
     requireDirectory(module,apiDirectory,{
       visit:whenLoadModule
     })
